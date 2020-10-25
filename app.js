b937b7c7bde0b6f967d4a352baaf553aa612882f
Faizan <faizanhuda12@gmail.com>
2020-10-25 18:52:45 -0400

    fixed some bugs

---
+++ app.js
@@ -0,0 +1,51 @@
+console.log("hi")
+
+AOS.init({
+  duration: 1400,
+})
+
+jQuery(document).ready(function(){
+	jQuery('.skillbar').each(function(){
+		jQuery(this).find('.skillbar-bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},6000);
+	});
+});
+
+
+var docWidth = document.documentElement.offsetWidth;
+
+[].forEach.call(
+  document.querySelectorAll('*'),
+  function(el) {
+    if (el.offsetWidth > docWidth) {
+      console.log(el);
+    }
+  }
+);
+
+VANTA.DOTS({
+  el: "#contact",
+  mouseControls: true,
+  touchControls: true,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x113406,
+  color2: 0xffffff,
+  backgroundColor: 0xffffff
+})
+
+
+VANTA.NET({
+  el: "#anime",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  backgroundColor: 0x0
+})
